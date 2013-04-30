56e5a0cd809f2ccb2412d75d56539aeb66a19fa8
Steve Halford <steve.halford@boxuk.com>
2013-04-30 09:17:33 +0100

    Only check for a url if the bot is mentioned in a message

--- rodbot.js
+++ rodbot.js
@@ -15,6 +15,11 @@ bot.addListener('error', function(message) {
 });
 
 bot.addListener("message"+config.channel, function(from, text, message) {
+    //only continue if mentioned
+    if (text.indexOf(config.botName) === -1) {
+        return false;
+    }
+
     var exp = new RegExp(
         "(^|[ \t\r\n])((http|https):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))"
         ,"g"
